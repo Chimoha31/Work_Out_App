5c1180c1fe495556cecececbe0bb639d7b001f7f
Chimoha31 <mokochii1108@gmail.com>
2022-11-16 11:34:41 -0800

    init

---
+++ 01chat_firebase/src/App.js
@@ -0,0 +1,12 @@
+import Resister from './pages/Resister'
+
+const App = () => {
+  return (
+    <>
+    <Resister />
+      App Test Errorの有無確認
+    </>
+  )
+}
+
+export default App
---
+++ 01chat_firebase/src/pages/Resister.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resister = () => {
+  return (
+    <div className="formContainer">
+      Resister
+    </div>
+  )
+}
+
+export default Resister
