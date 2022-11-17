7e988e8e34c6a2b75600532b3c6899cebf7b592a
Chimoha31 <mokochii1108@gmail.com>
2022-11-17 15:10:40 -0800

    Implemented JS

--- 01chat_firebase/src/components/Search.jsx
+++ 01chat_firebase/src/components/Search.jsx
@@ -1,21 +1,63 @@
-import React from "react";
-import '../style.scss';
+import React, { useState } from "react";
+import "../style.scss";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    // Firebase -> search -> queryで検索
+    // 1.queryを作る
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    // 2.queryを作ったら検索された結果をget()する
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        console.log(doc.data());
+      });
+    } catch (e) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = () => {
+    
+  }
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a user" />
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/235805/pexels-photo-235805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
+        <input
+          type="text"
+          placeholder="Find a user"
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
         />
-        <div className="userChatInfo">
-          <span>Teddy</span>
-        </div>
       </div>
+      {err && <span>User not founded</span>}
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img
+            src="https://images.pexels.com/photos/235805/pexels-photo-235805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
