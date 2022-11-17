d398037ce860a43958bb611e541240f86ea692cf
Chimoha31 <mokochii1108@gmail.com>
2022-11-16 17:28:30 -0800

    Messages

--- 01chat_firebase/src/components/Input.jsx
+++ 01chat_firebase/src/components/Input.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import file_icon from '../img/file_icon.png';
+import attach from '../img/attach.png';
 
 const Input = () => {
   return (
     <div className="input">
-      Input
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <img src={attach} alt=""/>
+        <input type="file" id="file" style={{display: "none"}} />
+        <label htmlFor="file">
+          <img src={file_icon} alt=""/>
+        </label>
+        <button>Send</button>
+      </div>
     </div>
   )
 }
--- 01chat_firebase/src/components/Message.jsx
+++ 01chat_firebase/src/components/Message.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className="message">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/235805/pexels-photo-235805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img
+          src="https://images.pexels.com/photos/235805/pexels-photo-235805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
