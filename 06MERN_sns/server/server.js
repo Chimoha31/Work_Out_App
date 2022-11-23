b9237e4b8b28b09d16415cadc6033f7a3fc84355
Chimoha31 <mokochii1108@gmail.com>
2022-11-22 19:17:00 -0800

    Setting router

--- 06MERN_sns/server/server.js
+++ 06MERN_sns/server/server.js
@@ -1,1 +1,17 @@
-console.log("Test");
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/users");
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/posts");
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello server.js");
+});
+
+// middleware
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+
+app.listen(PORT, () => console.log("Server is running 👻"));
